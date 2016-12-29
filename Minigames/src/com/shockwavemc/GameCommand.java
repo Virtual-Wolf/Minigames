09511c6fe7bd3dc9a1442755b5334bbf3183d718
Virtual-Wolf <harryryan2012@gmail.com>
2016-12-29 09:59:40 +0000

    \o/

--- Minigames/src/com/shockwavemc/GameCommand.java
+++ Minigames/src/com/shockwavemc/GameCommand.java
@@ -1,8 +1,11 @@
 package com.shockwavemc;
 
+import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class GameCommand implements CommandExecutor {
 	public GameCommand(Minigames mgs) {
@@ -11,7 +14,44 @@ public class GameCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		// TODO Auto-generated method stub
+		Player p = null;
+		boolean isPlayer = false;
+		if(sender instanceof Player) {
+			p = (Player) sender;
+			isPlayer = true;
+		}
+		if(args.length == 0 || args[0].equalsIgnoreCase("help")) {
+			sender.sendMessage("" + ChatColor.GOLD + ChatColor.ITALIC + "Minigames Commands:\n"
+					+ "/mg join" + ChatColor.GRAY + ": Join the minigames\n"
+					+ "/mg leave" + ChatColor.GRAY + ": Leave the minigames\n"
+					+ "/mg help" + ChatColor.GRAY + ": Display this help menu\n"
+					+ "/mg force" + ChatColor.GRAY + ": Force a certain game to be chosen\n     " + ChatColor.RED  + ChatColor.BOLD + "OPS ONLY\n"
+					+ "/mg start" + ChatColor.GRAY + ": Make the minigames start forcibly     " + ChatColor.RED  + ChatColor.BOLD + "OPS ONLY\n"
+					+ "/mg stop" + ChatColor.GRAY + ": Make the minigames stop forcibly     " + ChatColor.RED  + ChatColor.BOLD + "OPS ONLY\n");
+			return false;
+		} else {
+			if(args[0].equalsIgnoreCase("join")) {
+				if(args.length > 1) {
+					sender.sendMessage(ChatColor.RED + "Incorrect Syntax. Use /mg join");
+					return false;
+				}
+				if(!isPlayer) {
+					sender.sendMessage(ChatColor.RED + "Only players can join minigames!");
+					return false;
+				}
+				if(!Minigames.inGame) {
+					Minigames.getPlayerList().add(p.getName());
+					p.teleport(Minigames.spawn);
+					p.setGameMode(GameMode.ADVENTURE);
+					p.setFlying(false);
+					p.sendMessage(ChatColor.GRAY + "Minigames will start shortly!");
+					if(!Minigames.starting) {
+						Minigames.run();
+					}
+					return false;
+				}
+			}
+		}
 		return false;
 	}
 
--- Minigames/src/com/shockwavemc/Minigames.java
+++ Minigames/src/com/shockwavemc/Minigames.java
@@ -1,9 +1,11 @@
 package com.shockwavemc;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.*;
 
@@ -11,8 +13,10 @@ public class Minigames extends JavaPlugin {
 	private ScoreboardManager sMan = Bukkit.getScoreboardManager();
 	private static Scoreboard board;
 	private static Game current;
-	public static boolean inGame = false;
+	public static boolean inGame, starting = false;
 	private static ArrayList<String> participating = new ArrayList<String>();
+	private static ArrayList<GameTeam> activeTeams = new ArrayList<GameTeam>();
+	public static Location spawn = new Location(Bukkit.getWorld("Minigames"), 0, 100.5, 0);
 	
 	public void onEnable() {
 		board = sMan.getNewScoreboard();
@@ -36,14 +40,26 @@ public class Minigames extends JavaPlugin {
 		return current;
 	}
 	
-	public static void Broadcast(String message) {
-		Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "" + ChatColor.GOLD + ChatColor.ITALIC + "Minigames:" + ChatColor.GRAY + " "));
+	public static void Broadcast(boolean prefix, String message) {
+		Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', prefix ? ("" + ChatColor.GOLD + ChatColor.ITALIC + "Minigames:") : ("") + ChatColor.GRAY + " "));
 	}
 	
+	private static void getNewGame() {
+		Game g = Game.values()[r.nextInt(Game.values().length)];
+		/*while(g == current) {
+			g = Game.values()[r.nextInt(Game.values().length)];
+		}*/ // To implement once there's atleast 2 games
+		current = g;
+	}
+	
+	private static Random r = new Random();
 	public static void run() {
 		if(participating.size() < 2) {
-			Broadcast("Not enough players to start!");
+			Broadcast(true, "Not enough players to start!");
 			return;
 		}
+		starting = true;
+		getNewGame();
+		Broadcast(false, "&e&l" + current.name + " &7" + ChatColor.ITALIC + " has been chosen as the next game.");
 	}
 }
